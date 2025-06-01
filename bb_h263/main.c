9c5108a147ae6154ef3cf22d8d473d7c00d4109e
Larry Bank <bitbank@pobox.com>
2025-06-01 20:38:51 +0100

    Initial Commit

---
+++ bb_h263/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  bb_h263
+//
+//  Created by Laurence Bank on 01/06/2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
