56945a34ed43e686cfda77907e3bef01949d2c03
Larry Bank <bitbank@pobox.com>
2026-03-29 12:59:02 -0400

    Added ESP32 example sketch

---
+++ examples/esp32_player/esp32_player.ino
@@ -0,0 +1,77 @@
+//
+// H.263 Player example
+//
+#include <bb_spi_lcd.h>
+#include <bb_h263.h>
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+#include "homer.h"
+
+BB_H263 h263;
+BB_SPI_LCD lcd;
+TaskHandle_t myTaskHandle = NULL;
+volatile bool bReady = false;
+int xoff, yoff, w, h;
+uint8_t *pBuf;
+
+void ShowFrame(void *p)
+{
+  while (1) {
+    if (bReady) {
+       lcd.setAddrWindow(xoff, yoff, w, h);
+       lcd.pushPixels((uint16_t *)pBuf, w*h);
+       bReady = false;
+    } else {
+      vTaskDelay(1);
+    }
+  }
+} /* ShowFrame() */
+
+void setup()
+{
+  int rc;
+  int iFrame;
+  
+  Serial.begin(115200);
+  delay(3000);
+  Serial.println("Starting H.263 demo");
+ 
+ // Arduino runs on core 1, so pin this task to core 0
+  xTaskCreatePinnedToCore(ShowFrame, "Show Frame", 4096, NULL,10, &myTaskHandle, 0);
+
+  lcd.begin(DISPLAY_WS_AMOLED_18);
+  lcd.fillScreen(TFT_BLACK);
+  
+  rc = h263.open(homer_car_h263, (int)sizeof(homer_car_h263));
+  if (rc == H263_SUCCESS) {
+      h263.setPixelType(H263_PIXEL_RGB565_BE);
+      w = h263.getWidth();
+      h = h263.getHeight();
+      Serial.printf("File opened: %d x %d, %d frames\n", w, h, h263.getFrameCount());
+      xoff = (lcd.width() - w)/2;
+      yoff = (lcd.height() - h)/2;
+      pBuf = (uint8_t *)malloc(w * h *2);
+      h263.setFrameBuf(pBuf, w * 2);
+      for (int iLoop = 0; iLoop <5; iLoop++) {
+        iFrame = 0;
+        rc = H263_SUCCESS;
+        while (rc == H263_SUCCESS) {
+            rc = h263.decodeFrame(0, 0);
+            while (bReady) {
+              delay(1); // wait for display thread to finish
+            }
+            bReady = true; // draw it
+            //ShowFrame(pBuf, w, h, xoff, yoff);
+            iFrame++;
+        }
+      }
+      Serial.println("About to call close");
+      h263.close();
+      Serial.println("Returned from close");
+  }
+}
+
+void loop()
+{
+}
+
